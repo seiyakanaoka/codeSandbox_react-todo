f535703b4481d2710669dfe0cce62be0e7e4260e
seiyakanaoka <78468016+seiyakanaoka@users.noreply.github.com>
2021-09-05 16:29:50 +0900

    update Componentization of BackTodos

---
+++ src/components/BackTodos.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const BackTodos = (props) => {
+  const { completeTodos, onClickBack } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {completeTodos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickBack(index)}>戻る</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
